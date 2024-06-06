fac94bb30b963d2dc83ee3235de3e09d76a0605d
Ritvik Thakur <ritvik.thakur04@gmail.com>
2024-06-05 21:35:23 -0400

    fixed submodule problem

---
+++ web/src/Pages/Upload.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../App.css';
+
+
+const Upload = () => {
+    return (
+        <div>
+            <h1>
+                This is where you will uplaod a picture of your clothes
+            </h1>
+        </div>
+    )
+};
+
+export default Upload;
